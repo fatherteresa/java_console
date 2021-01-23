cdf900f075404b7bb8dcf5879995dbe45e7df0b5
ASUS ROG G15DH <ASUS ROG G15DH@192.168.1.8>
2021-01-24 07:14:35 +0900

    패키지명 수정

---
+++ java_console/src/A0_Basic_이론/A2_Print4_서식문자.java
@@ -0,0 +1,41 @@
+package A0_Basic_이론;
+
+import java.util.Scanner;
+/*
+ * # 출력문의 종류
+ * 1. System.out.println();
+ * 1) ln = new line
+ * 2) 줄바꿈 가능
+ * 2. System.out.print();
+ * 1) 줄바꿈 불가
+ * 3. System.out.printf();
+ * 1) 서식문자 출력
+ * 2) 종류
+ * . %d : 정수
+ * . %f : 소수
+ * . %c : 문자 1개
+ * . %s : 문자 여러개
+ */
+public class A2_Print4_서식문자 {
+	public static void main(String[] args) {
+		
+		System.out.println("-------------------------------");
+		System.out.printf("%d", 10);
+		System.out.println();
+		System.out.println("-------------------------------");
+		System.out.printf("%f\n", 3.14);
+		
+		System.out.printf("%.2f\n", 3.14); // 소수점 개수를 제어할수있다. 
+		
+		System.out.println("-------------------------------");
+		System.out.printf("%c\n", 'b');
+		
+		System.out.printf("%s\n", "출력문의이해");
+		System.out.println("-------------------------------");
+		String fruit = "사과";
+		int cnt = 5;
+		System.out.printf("%s가 %d개 있습니다.\n", fruit, cnt);
+		System.out.println("-------------------------------");
+
+	}
+}
---
+++ java_console/src/D0_if_이론/D_if6_조건문옵션else.java
@@ -0,0 +1,34 @@
+package D0_if_이론;
+
+import java.util.Random;
+
+public class D_if6_조건문옵션else {
+	public static void main(String[] args) {
+
+		/*
+		  	if 문의 옵션 2종류  (1) else (2) else if 
+			
+			# (1) 조건이 2가지인경우 else 를 이용해서 2번째 조건을 생략할수있다. 
+			
+		 */
+		
+		// 예) 홀짝출력.
+		int num = 11;
+		if (num % 2 == 0) {
+			System.out.println("짝수");
+		}
+		if (num % 2 == 1) {
+			System.out.println("홀수");
+		}
+
+		System.out.println("-------------------------");
+
+		if (num % 2 == 0) {
+			System.out.println("짝수");
+		} 
+		else { // if의 조건이 아니면 무조건실행한다. 
+			System.out.println("홀수");
+		}
+
+	}
+}
