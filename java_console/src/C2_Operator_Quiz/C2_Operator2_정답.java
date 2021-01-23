d686fbb537fd4f594bdbf85f6990b0c7b357114b
ASUS ROG G15DH <ASUS ROG G15DH@192.168.1.8>
2021-01-24 07:10:26 +0900

    D4 파일이름변경

---
+++ java_console/src/C2_Operator_Quiz/C2_Operator2_정답.java
@@ -0,0 +1,18 @@
+package C2_Operator_Quiz;
+
+public class C2_Operator2_정답 {
+	public static void main(String[] args) {
+		
+		int a = 1;
+		int b = 2;
+		int c = 3;
+		int d = 4;
+		// 아래 값이 전부 True 가 나오도록 수를 변경해보세요.
+		
+		System.out.println(a * d == c + b &&  a > b);
+		System.out.println(a / d + 3 == c - b &&  a > b);
+		System.out.println(a % d == c % b &&  a > b);
+		System.out.println((a - d) * 3 == c / 3 + b &&  a > b);
+	
+	}
+}
